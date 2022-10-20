c09c75d5df320a9864de51ee2f1ecd4ffb512607
quintinmp <paviloni5@gmail.com>
2022-10-20 14:17:39 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let weather = {
+    apiKey:"e862205d7adcdfab736e170ee464af7d",
+    fetchWeather: function (city) {
+        fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=imperial&appid=" 
+        + this.apiKey)
+       
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed} = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity +  "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km";
+        document.querySelector(".weather").classList.remove("loading");
+        
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") { 
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Denver");
